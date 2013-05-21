93c6c8a4ec87bf979bdb18dcdb61ba889973571c
Andrew <andrew.andrew.epifano0@gmail.com>
2013-05-20 23:29:11 -0400

    Base Plugin Components

--- Apocalypse/src/com/github/CastawayDevelopment/Apocalypse/Apocalypse.java
+++ Apocalypse/src/com/github/CastawayDevelopment/Apocalypse/Apocalypse.java
@@ -1,10 +1,23 @@
 package com.github.CastawayDevelopment.Apocalypse;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  * Time to start slaving over another project
  *
  */
 
-public class Apocalypse {
+public class Apocalypse extends JavaPlugin{
+	
+	
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
 
 }
