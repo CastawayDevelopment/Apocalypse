dd1c29ec433aa64db2bbe66d1e718a11530093e1
Andrew <andrew.andrew.epifano0@gmail.com>
2013-05-22 17:54:43 -0400

    Added Another Line of code :D

--- Apocalypse/src/com/github/CastawayDevelopment/Apocalypse/Apocalypse.java
+++ Apocalypse/src/com/github/CastawayDevelopment/Apocalypse/Apocalypse.java
@@ -4,7 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Time to start slaving over another project
- *
+ * Project Page: https://docs.google.com/document/d/18CVGaZsK8V-7hc0wSq2mwxmYAGiZ6ZxPZn4XE0rOun0/edit?usp=sharing
  */
 
 public class Apocalypse extends JavaPlugin{
